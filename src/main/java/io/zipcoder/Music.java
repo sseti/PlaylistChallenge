746e8e3040156ecc95e3b5c7db8b8258729b4acf
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro.local>
2020-02-16 11:10:32 -0500

    finished lab Sandy Setiawan

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int decrease = 0;
+        int increase = 0;
+
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i].equals(selection))
+                break;
+            if (i + 1 > playList.length)
+                i = 0;
+            decrease++;
+        }
+
+        for (int i = startIndex; i < playList.length; i--) {
+            if (playList[i].equals(selection))
+                break;
+            if (i - 1 < 0)
+                i = playList.length;
+            increase++;
+        }
+
+        if (decrease < increase) {
+            return decrease;
+        }
+        return increase;
+
     }
-}
+    }
+
